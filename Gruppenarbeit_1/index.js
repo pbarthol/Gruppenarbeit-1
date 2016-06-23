9bae28842f1b9b67807e87ddcbf3302c11e13f05
Peter Bartholet <peter.bartholet@kfnmail.ch>
2016-06-23 14:34:44 +0200

    added some files

---
+++ Gruppenarbeit_1/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var hbs = require('express-hbs');
+var session = require("express-session");
+
+var app = express();
+app.engine('hbs', hbs.express4());
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(require("cookie-parser")());
+app.use(session({ secret: 'casduichasidbnuwezrfinasdcvjkadfhsuilfuzihfioda', resave: false, saveUninitialized: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(require("method-override")(function(req, res){
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
+//app.use("/", require('./routes/indexRoutes.js'));
+app.use("/", require('./routes/notesRoutes.js'));
+app.use("/notes", require('./routes/notesRoutes.js'));
+app.use("/notes/edit", require('./routes/notesRoutes.js'));
+/*
+app.get("/ajax", function(req, res){
+    res.sendFile("./public/html/index.html")
+});
+*/
+app.use(express.static('public/html'));
+app.use(express.static('public/js'));
+app.use(express.static('public'));
+
+hbs.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b) // Or === depending on your needs
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
+const hostname = '127.0.0.1';
+const port = 3001;
+app.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}/`); });
