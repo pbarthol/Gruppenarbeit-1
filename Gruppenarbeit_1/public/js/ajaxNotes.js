7a2e1c423d8cfc0162b09330b4129b3ac95ea8ae
Peter Bartholet <peter.bartholet@kfnmail.ch>
2016-06-25 11:11:26 +0200

    moved ajaxs from main.js to ajaxNotes.js

---
+++ Gruppenarbeit_1/public/js/ajaxNotes.js
@@ -0,0 +1,94 @@
+function ajaxGetAllNotes(sb) {
+    $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "/notes",
+        //data: { id : 1 }
+    }).done(function (msg) {
+        notes = msg;
+        if ((notes) && (notes.length > 0)) {
+            sortby = (sb) ? sb : sortby;
+            switch (sortby) {
+                case 'dateDue':
+                    $("#notes").html(createNotesHtml(getNotesFiltered().sort(compareNotesDateDue)));
+                    break;
+                case 'dateCreated':
+                    $("#notes").html(createNotesHtml(getNotesFiltered().sort(compareNotesDateCreated)));
+                    break;
+                case 'priority':
+                    $("#notes").html(createNotesHtml(getNotesFiltered().sort(compareNotesPriority)));
+                    break;
+            }
+        } else {
+            notes = [];
+            $("#notes").html("<p>No notes found.</p>");
+        }
+    });
+}
+
+function ajaxSaveNote(note) {
+    // two functions:
+    // If note._id = "newNote" -> insert new Note
+    // Else -> update Note
+    if (note._id === "newNote") {
+        $.ajax({
+            dataType: "json",
+            method: "POST",
+            contentType: 'application/json',
+            url: "/note/",
+            data: JSON.stringify({"note": note})
+        }).done(function (msg) {
+            //note = jQuery.parseJSON(msg);
+            //renderEditor(note);
+            $("#note-editor").hide();
+            $("main").show();
+            $("#header-sorting").show();
+            renderSortedNotes();
+
+        }).fail(function (msg) {
+            output.text(JSON.stringify(msg));
+        });
+    }
+    else {
+        $.ajax({
+            dataType: "json",
+            method: "PUT",
+            contentType: 'application/json',
+            url: "/note/",
+            data: JSON.stringify({"note": note})
+        }).done(function (msg) {
+            $("#note-editor").hide();
+            $("main").show();
+            $("#header-sorting").show();
+            renderSortedNotes();
+        }).fail(function (msg) {
+            output.text(JSON.stringify(msg));
+        });
+    }
+    /*
+     $("#note-editor").hide();
+     $("main").show();
+     $("#header-sorting").show();
+     renderSortedNotes();
+     */
+}
+
+function ajaxDeleteNote(note){
+    $.ajax({
+        dataType: "json",
+        method: "POST",
+        contentType: 'application/json',
+        url: "/note/delete",
+        data: JSON.stringify({"note": note})
+    }).success(function (msg) {
+        $("#note-editor").hide();
+        $("main").show();
+        $("#header-sorting").show();
+        renderSortedNotes();
+    }).fail(function (msg) {
+        renderSortedNotes();
+    });
+    
+}
+
+
